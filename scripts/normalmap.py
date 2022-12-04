6a7d5087fe2008be75745af5c97a54c8e0fe8dc9
grae <hi@hi.com>
2022-12-04 01:45:14 -0600

    fix normal invert

--- scripts/normalmap.py
+++ scripts/normalmap.py
@@ -301,7 +301,7 @@ class Script(scripts.Script):
 				img_output = out.astype("uint16")
 
 				# invert normal map
-				if invert_normal ^ model_type == 4:
+				if not (invert_normal ^ model_type == 4):
 					img_output = cv2.bitwise_not(img_output)
 
 				img_output *= scale_depth
